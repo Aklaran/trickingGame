3fbd8e5e3d219ccc5018b65d449df3c5f269586c
Siri Tembunkiart <botbunk@gmail.com>
2017-04-25 01:29:43 -0400

    buttons

--- code/menu.py
+++ code/menu.py
@@ -6,19 +6,26 @@ class MainMenu(object):
 
         self.playButton = DirectButton(text=("Play"), scale = 0.25,
                      command=self.switchToPlay, parent=self.parentNode)
-        self.saveLoadButton = DirectButton(text=('Save/Load'), scale=0.25,
-                                           command=self.switchToSaveLoad, parent=self.parentNode,
-                                           pos=(0.0, 0, -0.5))
+        self.saveButton = DirectButton(text=('Save'), scale=0.25,
+                                           command=self.switchToSave, parent=self.parentNode,
+                                           pos=(0.0, 0, -0.325))
+        self.loadButton = DirectButton(text=('Load'), scale=0.25,
+                                       command=self.switchToLoad, parent=self.parentNode,
+                                       pos=(0.0, 0, -0.6))
         #DirectButton(text=("Save/Load"))
         #DirectButton(text=("My Tricker"))
        # DirectButton(text=("Options"))
 
+    def switchToLoad(self):
+        base.gameFSM.demand('Load')
+
     def switchToPlay(self):
         base.gameFSM.demand('Play')
 
-    def switchToSaveLoad(self):
-        base.gameFSM.demand('SaveLoad')
+    def switchToSave(self):
+        base.gameFSM.demand('Save')
 
     def destroy(self):
         self.parentNode.removeNode()
         self.playButton.removeNode()
+        self.saveButton.removeNode()
