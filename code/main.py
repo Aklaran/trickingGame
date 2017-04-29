236b7b2dba48b4d4e9f36921b4816ddbc15a6875
Aklaran <s.tembunk@gmail.com>
2017-04-29 19:15:55 -0400

    added dubcork anim

--- code/main.py
+++ code/main.py
@@ -8,8 +8,15 @@ from load import Load
 from stats import Stats
 from tricker import Tricker
 
+# TODO: All this shit
+"""
+Make a screen/menu class that has functions like popuptext, transitions, etc
+Add a late/early modifier to the timing
+Reformat UI
+dem animations
+    really good raiz: guthrie slow 2:27
+"""
 
-# TODO: Make a screen/menu class that has functions like popuptext, transitions, etc
 
 class Main(ShowBase):
     def __init__(self):
@@ -20,32 +27,43 @@ class Main(ShowBase):
         self.gameFSM = GameFSM('Core Game FSM')
         self.gameFSM.demand('MainMenu')
 
+
 class GameFSM(FSM):
     def enterMainMenu(self):
         self.menu = MainMenu()
+
     def exitMainMenu(self):
         self.menu.destroy()
         del self.menu
+
     def enterPlay(self):
         self.play = TrickingGame()
+
     def exitPlay(self):
         self.play.destroy()
         del self.play
+
     def enterSave(self):
         self.save = Save()
+
     def exitSave(self):
         self.save.destroy()
         del self.save
+
     def enterLoad(self):
         self.load = Load()
+
     def exitLoad(self):
         self.load.destroy()
         del self.load
+
     def enterStats(self):
         self.stats = Stats()
+
     def exitStats(self):
         self.stats.destroy()
         del self.stats
+
 
 app = Main()
 app.run()
